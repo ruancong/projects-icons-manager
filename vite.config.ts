3c52f1268146ef2d658997c150fb4a08183bf59e
RuanCong <1308811723@qq.com>
2024-12-13 16:21:45 +0800

    feature: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
