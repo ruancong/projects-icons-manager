626147dd6c5ef723cbd016ee77144d6dd1739020
RuanCong <1308811723@qq.com>
2024-12-14 09:39:35 +0800

    feature: 修改本地化

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,16 @@
-// import "~/styles/element/index.scss";
-
 import ElementPlus from 'element-plus';
-// import all element css, uncommented next line
-// import "element-plus/dist/index.css";
-
-// or use cdn, uncomment cdn link in `index.html`
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 
 import { routes } from 'vue-router/auto-routes';
 import App from './App.vue';
-
 import '~/styles/index.scss';
 
 import 'uno.css';
-// If you want to use ElMessage, import it.
-import 'element-plus/theme-chalk/src/message.scss';
-import 'element-plus/theme-chalk/src/message-box.scss';
+import 'element-plus/dist/index.css';
 
-// if you do not need ssg:
 import { createApp } from 'vue';
 import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
-import zhCn from 'element-plus/es/locale/lang/zh-cn';
-
 
 const app = createApp(App);
 app.use(
